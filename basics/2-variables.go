719dce6e28e127fe0e942ef7c0d9fb1c5a706007
Aswini S <ashumeow@live.com>
2015-03-31 22:05:13 +0530

    add variables

---
+++ basics/2-variables.go
@@ -0,0 +1,19 @@
+package main
+import "fmt"
+func main() {
+
+	var a string = "ashumeow"
+	fmt.Println(a)
+
+	var b, c, d int = 14, 05, 1991
+	fmt.Println(b, c, d)
+
+	var e = true
+	fmt.Println(e)
+
+	var f int
+	fmt.Println(f)
+
+	g := "athletic"
+	fmt.Println(g)
+}
